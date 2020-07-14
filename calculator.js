7a33c71a437cf28141c543f6ad2705b01e6136c4
Kevin <kevin03.nguyen@gmail.com>
2020-07-14 00:35:58 -0400

    added operator function and functio that displays buttons clicks

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
+let text =""
+window.onload = function(){
+    populate();
+}
+
+
 function add(...num){
     let total = num[0];
     for (let index = 1; index < num.length; index++) {
@@ -34,4 +40,28 @@ function divide(...num){
         
     }
 return total;
+}
+
+function operator(...op){
+    if(op[0] === "+"){
+        add(op[1], op[2]);}
+    else if(op[0] === "-"){
+        subtract(op[1], op[2]);}
+    else if(op[0] === "*"){
+        multiply(op[1], op[2]);}
+    else{
+        divide(op[1], op[2]);}
+}
+function display(){
+    
+    let screen = document.querySelector(".display");
+    screen.textContent += this.textContent;
+}
+
+function populate(){
+    let buttons = document.querySelectorAll(".bot");
+    buttons.forEach(function(ele){
+        ele.addEventListener("click", display);
+    })
+
 }
