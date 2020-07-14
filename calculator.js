723ea5f1eb31aa0d14f5e55022cf814a2c0960df
Kevin <kevin03.nguyen@gmail.com>
2020-07-13 22:56:01 -0400

    added all the operators +-/*

--- calculator.js
+++ calculator.js
@@ -15,3 +15,23 @@ function subtract(...num){
     }
 return total;
 }
+
+function multiply(...num){
+    let total = num[0];
+    for (let index = 1; index < num.length; index++) {
+        total *= num[index] ;
+        
+    }
+return total;
+}
+
+
+function divide(...num){
+    let total = num[0];
+    for (let index = 1; index < num.length; index++) {
+        if (num[index] === 0){return alert("not allowed!")}
+        total /= num[index] ;
+        
+    }
+return total;
+}
